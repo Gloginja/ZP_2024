66fe6d9fda230fdc7c0790855f55a021c52a26fb
Gloginja <amarko00@hotmail.com>
2024-06-03 17:58:25 +0200

    Revert "Added generating PEM files"
    This reverts commit 283929c8d80eddbc0d77cbe85b605e6a32123c82.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ users.append(gloginja)
 milena = User('jmilena3110@gmail.com', 'milena123', 'Milena')
 users.append(milena)
 
-gloginja_keyID = km.generateNewPairRSA(1024, gloginja.email, 'markog99', 2)
+gloginja_keyID = km.generateNewPairRSA(1024, gloginja.email, 'markog99')
 
-milena_keyID = km.generateNewPairRSA(1024, milena.email, 'milena123', 2)
+milena_keyID = km.generateNewPairRSA(1024, milena.email, 'milena123')
 
 msg = PGPMessage(datetime.datetime.now(), 'Milena sta radis')
 #msg.save(filePath='test.json', PR=km.getPR(gloginja_keyID, 'markog99'), PU=km.getPU(milena_keyID), isCompressed=True, algo=1, keyID=milena_keyID)
@@ -28,5 +28,9 @@ msg = PGPMessage()
 msg.load(filePath='test.json', PR=km.getPR(milena_keyID, 'milena123'), PU=km.getPU(gloginja_keyID))
 
 print(msg.message['data'])
-print(km.load_keys_from_directory('.'))
+
+saveKey('test.pem','jmilena3110@gmail.com', km.getPU(milena_keyID))
+
+pu = loadKey('test.pem','jmilena3110@gmail.com')
+
 pass
